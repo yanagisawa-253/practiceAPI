b1e512933b267fd0deda7519d12492910da8aa43
yanagisawa-253 <anaguma253@gmail.com>
2021-12-28 13:30:17 +0000

    [update]予約機能の為の準備(book投稿機能)

---
+++ Api/app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
