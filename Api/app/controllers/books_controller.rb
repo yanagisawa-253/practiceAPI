7437e92f9946025ca32189844bbaa6f8dced5bf5
yanagisawa-253 <anaguma253@gmail.com>
2021-12-28 13:39:03 +0000

    [update]devise機能の追加

--- Api/app/controllers/books_controller.rb
+++ Api/app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
