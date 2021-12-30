94864dbb0e7b2fb5cc4bf19d969a8aa30abc6542
yanagisawa-253 <anaguma253@gmail.com>
2021-12-30 11:08:09 +0000

    [update]user機能

--- Api/app/controllers/users_controller.rb
+++ Api/app/controllers/users_controller.rb
@@ -1,24 +1,60 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
-    @tasks = current_user.tasks
+    @book = @user.books
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    if @user.save
+      flash[:notice] = "You have save user successfully."
+      redirect_to books_path
+    else
+       flash.now[:notice] = "You have user error successfully."
+      render :index
+    end
   end
 
   def edit
-    @user = User.find(params[:id])
+     @user = User.find(params[:id])
+     if @user == current_user
+       render "edit"
+     else
+        @user = current_user
+        redirect_to user_path(current_user.id)
+     end
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    flash[:notice] = "プロフィールを更新しました"
-    redirect_to user_path(@user)
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:notice] = "You have updated user successfully."
+       redirect_to user_path(@user.id)
+      else
+       flash.now[:notice] = "You updated error successfully."
+       render :edit
+      end
+    else
+      redirect_to books_path
+    end
   end
+
+  def destroy
+    @user.destroy
+    redirect_to :homes => :top
+  end
+
 
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:email)
   end
-
 end
