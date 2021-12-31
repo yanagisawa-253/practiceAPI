a807d9827987010348189432c36f6379812f31b9
yanagisawa-253 <anaguma253@gmail.com>
2021-12-31 10:40:03 +0000

    [update]user,book投稿機能完了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @book = @user.books
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    if @user.save
+      flash[:notice] = "You have save user successfully."
+      redirect_to books_path
+    else
+       flash.now[:notice] = "You have user error successfully."
+      render :index
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :body)
+  end
+end
